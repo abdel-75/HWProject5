a3f3ff51da4481472116532a6cc751b773cff271
Abdessalam <Abdessalam@192.168.148.117>
2023-04-07 20:13:38 +0000

    fifth Commit

---
+++ src/hwtest5/DemoProject5.java
@@ -0,0 +1,11 @@
+package hwtest5;
+
+public class DemoProject5 {
+	public static void main(String[] args) {
+		System.out.println("this is demo project 5");
+		
+		
+		System.out.println("this is porject 5");
+	}
+
+}
